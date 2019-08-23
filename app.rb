79f944822c7c0bef54d643fd10620812dfa4fc53
Leslie Osorio <lesosorio17@gmail.com>
2019-08-23 18:54:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,12 +9,20 @@ class App < Sinatra::Base
   # Add your post route and action below
 
   get '/' do
+<<<<<<< HEAD
    "hello world"
+=======
+    "hello world"
+>>>>>>> 6b30a37286d8ee5c9463ce0a9ac813fa2d22bca5
   end
 
   post '/food' do
     #params.to_s
+<<<<<<< HEAD
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+=======
+    params[:favorite_food].to_s
+>>>>>>> 6b30a37286d8ee5c9463ce0a9ac813fa2d22bca5
   end
 
 end
